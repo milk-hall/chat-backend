0a57c31066181b4b9717f35637690dba98d06621
wucaixin <297912123@qq.com>
2020-07-22 22:14:31 +0800

    feat: 增加跨域和ws

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,16 +1,32 @@
-import { EggAppConfig, EggAppInfo, PowerPartial } from 'midway';
+import { EggAppConfig, EggAppInfo, PowerPartial } from "midway";
 
-export type DefaultConfig = PowerPartial<EggAppConfig>
+export type DefaultConfig = PowerPartial<EggAppConfig>;
 
 export default (appInfo: EggAppInfo) => {
   const config = {} as DefaultConfig;
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_{{keys}}';
+  config.keys = appInfo.name + "_{{keys}}";
 
   // add your config here
-  config.middleware = [
-  ];
-
+  config.middleware = [];
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
+  config.cors = {
+    origin: "*", // 匹配规则  域名+端口  *则为全匹配
+    allowMethods: "GET,HEAD,PUT,POST,DELETE,PATCH",
+  };
+  config.io = {
+    init: {}, // passed to engine.io
+    namespace: {
+      "/": {
+        connectionMiddleware: [],
+        packetMiddleware: [],
+      },
+    },
+  };
   return config;
 };
--- src/config/plugin.ts
+++ src/config/plugin.ts
@@ -1,4 +1,12 @@
-import { EggPlugin } from 'midway';
+import { EggPlugin } from "midway";
 export default {
   static: true, // default is true
+  cors: {
+    enable: true,
+    package: "egg-cors",
+  },
+  io: {
+    enable: true,
+    package: "egg-socket.io",
+  },
 } as EggPlugin;
